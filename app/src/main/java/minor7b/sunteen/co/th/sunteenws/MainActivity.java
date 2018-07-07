38c412c65931ec05a48c3b034ecb4f96380dbd6a
jade <jade.yo@sunteen.co.th>
2018-07-07 12:47:17 +0700

    add fragment completed

--- app/src/main/java/minor7b/sunteen/co/th/sunteenws/MainActivity.java
+++ app/src/main/java/minor7b/sunteen/co/th/sunteenws/MainActivity.java
@@ -3,11 +3,24 @@ package minor7b.sunteen.co.th.sunteenws;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import minor7b.sunteen.co.th.sunteenws.fragment.MainFragment;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
-}
+
+//        Add Fragment to Activity
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.contentMainFragment, new MainFragment()).commit();
+
+        }
+
+
+
+    } // Main Method
+
+} // Main Class
