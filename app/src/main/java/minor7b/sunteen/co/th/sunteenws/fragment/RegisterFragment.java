5a161a80abfc7d78da50f94e8f5068fe4b793fd7
jade <jade.yo@sunteen.co.th>
2018-07-08 11:32:27 +0700

    Start 8 July

--- app/src/main/java/minor7b/sunteen/co/th/sunteenws/fragment/RegisterFragment.java
+++ app/src/main/java/minor7b/sunteen/co/th/sunteenws/fragment/RegisterFragment.java
@@ -1,5 +1,9 @@
 package minor7b.sunteen.co.th.sunteenws.fragment;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -8,13 +12,23 @@ import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 import minor7b.sunteen.co.th.sunteenws.MainActivity;
 import minor7b.sunteen.co.th.sunteenws.R;
+import minor7b.sunteen.co.th.sunteenws.utility.MyAlertDialog;
 
 public class RegisterFragment extends Fragment{
+
+    //    Explicit
+    private Uri uri;
+    private ImageView imageView;
+    private boolean aBoolean = true;
 
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
@@ -23,10 +37,102 @@ public class RegisterFragment extends Fragment{
 //        Create Toolbar
         createToolbar();
 
-
+//        Picture Controller
+        pictureController();
 
 
     }   // Main Method
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        if (item.getItemId() == R.id.itemUpload) {
+
+            uploadAndUpdateValue();
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void uploadAndUpdateValue() {
+
+        MyAlertDialog myAlertDialog = new MyAlertDialog(getActivity());
+        EditText nameEditText = getView().findViewById(R.id.edtName);
+        EditText userEditText = getView().findViewById(R.id.edtUser);
+        EditText passwordEditText = getView().findViewById(R.id.edtPassword);
+
+//        get Value to String
+        String nameString = nameEditText.getText().toString().trim();
+        String userString = userEditText.getText().toString().trim();
+        String passwordString = passwordEditText.getText().toString().trim();
+
+
+
+//        Check Choose Image and Space
+        if (aBoolean) {
+//            Chose None
+
+            myAlertDialog.normalDialog("No Image Selected","Please Select Image");
+        } else if (nameString.isEmpty() || userString.isEmpty() || passwordString.isEmpty()) {
+//            space detected
+            myAlertDialog.normalDialog("Blank Detected","Please Fill All Fields");
+
+        } else {
+
+        }
+
+
+
+    } // upload update
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (resultCode == getActivity().RESULT_OK) {
+
+//            Success Select Image
+            uri = data.getData();
+            aBoolean = false;
+
+//            Show Image on ImageView
+            try {
+
+                Bitmap bitmap = BitmapFactory.decodeStream(getActivity().getContentResolver().openInputStream(uri));
+                imageView.setImageBitmap(bitmap);
+
+
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+
+        } else {
+            Toast.makeText(getActivity(), "Please Select Image", Toast.LENGTH_SHORT).show();
+        }
+
+    }
+
+    private void pictureController() {
+        imageView = getView().findViewById(R.id.imvPicture);
+        imageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+//                Intent to Other App
+                Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+                intent.setType("image/*");
+                startActivityForResult(Intent.createChooser(intent, "Please Choose Image Viewer App"),
+                        1);
+
+
+
+            }
+        });
+
+
+    }
 
     @Override
     public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
